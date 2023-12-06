d70b8be52d15d3ac392a5f49d448ec31f949b623
ddaaggeett <david.g.daggett@gmail.com>
2023-12-06 11:16:22 -0500

    using wget instead of curl for OS config

--- src/debian/index.sh
+++ src/debian/index.sh
@@ -11,22 +11,23 @@ sudo apt install gphoto2 -y
 sudo apt install git -y
 
 # nvm - node.js
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-exec bash # TODO: does this work? reopens bash?
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+exec bash
 nvm install --lts
 
 # yt-dlp
-sudo wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /usr/local/bin/yt-dlp
+wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /usr/local/bin/yt-dlp
 sudo chmod a+rx /usr/local/bin/yt-dlp  # Make executable
 
 # install brave-browser
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo wget -O /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
-sudo apt install brave-browser
+sudo apt install brave-browser -y
 
 # install atom text editor
-curl -O https://github.com/atom/atom/releases/download/v1.60.0/atom-amd64.deb
+cd ~/Downloads
+wget https://github.com/atom/atom/releases/download/v1.60.0/atom-amd64.deb
 sudo dpkg -i atom-amd64.deb
 sudo apt-get install -f
 
